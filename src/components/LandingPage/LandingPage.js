82e00d88d23cac452404b985f2496fc26d82b8df
dayton brock <dayton.brock@gmail.com>
2020-04-14 12:18:14 -0500

    add artwork popup modal

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -5,7 +5,7 @@ import {
   Redirect,
   Switch,
 } from "react-router-dom";
-import { Card, Image, Button } from "semantic-ui-react";
+import { Header, Card, Image, Button, Modal } from "semantic-ui-react";
 
 import { connect } from "react-redux";
 
@@ -28,16 +28,26 @@ class LandingPage extends Component {
               <Card.Group stackable="true" centered="true">
                 {this.props.reduxState.setArt.map((item) => {
                   return (
-                    <Card>
-                      <Image src={item.image_url} />
-                      <Card.Content textAlign="right">
-                        <Card.Header>{item.title}</Card.Header>
-                        <Card.Description>{item.artist}</Card.Description>
-                      </Card.Content>
-                    </Card>
+                    <Modal closeIcon
+                        trigger={
+                            <Card>
+                                <Image src={item.image_url} />
+                                <Card.Content textAlign="right">
+                                    <Card.Header>{item.title}</Card.Header>
+                                    <Card.Description>{item.artist}</Card.Description>
+                                </Card.Content>
+                            </Card>
+                    }>
+                        <Modal.Content image className="artwork-popup">
+                            <Image wrapped-size="medium" src={item.image_url}/>
+                            <Header>{item.title}</Header>
+                            <p>by {item.artist}</p>
+                        </Modal.Content>
+                    </Modal>
                   );
                 })}
               </Card.Group>
+              
             </div>
             <div className="vote-button">
               <Button size="large">Cast Your Vote!</Button>
