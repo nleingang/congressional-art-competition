7720b7c72deee2c2f7a2239de537cc8182f189f4
dayton brock <dayton.brock@gmail.com>
2020-04-16 13:14:53 -0500

    create admin header and image upload sections, make header global font-size bigger

--- src/components/AdminPortal/AdminPortal.js
+++ src/components/AdminPortal/AdminPortal.js
@@ -5,7 +5,7 @@ import React, { Component } from "react";
 //   Redirect,
 //   Switch,
 // } from "react-router-dom";
-import { Button } from "semantic-ui-react";
+import { Button, Icon } from "semantic-ui-react";
 
 import { connect } from "react-redux";
 
@@ -22,8 +22,46 @@ class LandingPage extends Component {
     render() {
 
         return (
-          <div className="admin-portal-wrapper">
-            
+          <div class="admin-portal-wrapper">
+
+              <section class="admin-header">
+                <h1>Admin Portal</h1>
+                <h4 class="user-greeting">Welcome, { this.props.reduxState.user.username }!</h4>
+              </section>
+
+              <section class="admin-upload">
+                <h2>Image Upload</h2>
+                <div class="new-image-wrapper">
+                    <div class="all-inputs">
+                        <div>
+                            <button class="ui button">
+                                <Icon name="arrow up" positive/>Upload
+                            </button>
+                        </div>
+                        <div class="text-inputs">
+                            <div>
+                                <label for="title">Title:</label>
+                                <div class="ui input">
+                                    <input type="text" id="title" placeholder="Title..."/>
+                                </div>
+                            </div>
+
+                            <div>
+                                <label for="artist">Artist:</label>
+                                <div class="ui input">
+                                    <input type="text" id="artist" placeholder="Artist..."/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="submit-btn">
+                        <button class="ui green button">
+                            Submit
+                        </button>
+                    </div>
+                </div>
+              </section>
           </div>
         );
     }
