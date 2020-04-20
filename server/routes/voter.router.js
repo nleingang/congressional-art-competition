e8e2379f3daa62f8d7a8df2e3f72f8dcf885b623
dayton brock <dayton.brock@gmail.com>
2020-04-20 15:58:37 -0500

    create GET route to see if email is already in use

--- server/routes/voter.router.js
+++ server/routes/voter.router.js
@@ -1,52 +1,65 @@
 const express = require("express");
 const pool = require("../modules/pool");
+
 const router = express.Router();
 
 const { check, validationResult } = require('express-validator');
 
-/**
- * GET all art
- *
- * returns all artwork to be displayed on landing page
-//  */
-// router.get("/", (req, res) => {
-
-//     queryText = 'SELECT * FROM "artwork"';
-//     pool
-//         .query(queryText)
-//         .then((result) => {
-//             res.send(result.rows);
-//         })
-//         .catch((err) => {
-//             console.log("Error completing GET admin auctions query:", err);
-//             res.sendStatus(500);
-//         });
-// });
+// this will check the database to see if the email is already in use
+router.get("/", (req, res) => {
+    pool.query(`
+    SELECT email FROM voters WHERE email=$1
+    `, [req.body.email])
+        .then((result) => {
+            console.log("this is the number of rows:", result.rows.length)
+            if (result.rows.length != 0) {
+                res.send(true);
+                res.sendStatus(201);
+            } else {
+                res.send(false);
+                res.sendStatus(500);
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+});
 
 /**
  * POST route for new voter 
  */
-router.post("/add", (req, res) => {
+// router.post("/add", async (req, res, next) => {
+//     await check('email')
+//         .isEmail()
+//         .normalizeEmail()
+    
 
+//     await 
+//              // end custom validation
 
+//     errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//         return res.status(422).json({
+//             errors: errors.array()
+//         });
+//     }
 
-    console.log("voter route hit with req:", req.body);
-    pool
-        .query(
-            `
-  INSERT INTO "voters" ("email", "zip")
-  VALUES ($1,$2);
-  `,
-            [req.body.email, req.body.zip]
-        )
-        .then((result) => {
-            console.log(result);
-            res.sendStatus(201);
-        })
-        .catch((error) => {
-            console.log("error with adding voter:", error);
-            res.sendStatus(500);
-        });
-});
+//     console.log("voter route hit with req:", req.body);
+//     pool.query(
+//             `
+//   INSERT INTO "voters" ("email", "zip")
+//   VALUES ($1,$2);
+//   `,
+//             [req.body.email, req.body.zip]
+//         )
+//         .then((result) => {
+//             console.log(result);
+//             res.sendStatus(201);
+//         })
+//         .catch((error) => {
+//             console.log("error with adding voter:", error);
+//             res.sendStatus(500);
+//         });
+// });
 
 module.exports = router;
