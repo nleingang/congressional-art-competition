556662f875a9b60f7442e7953e4591c3dcbb89a2
dayton brock <dayton.brock@gmail.com>
2020-04-20 09:28:22 -0500

    save WIP before attempting an alternate solution

--- src/components/ArtCard/ArtCard.js
+++ src/components/ArtCard/ArtCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Header, Card, Image, Button } from "semantic-ui-react";
+import { Card, Image, Button } from "semantic-ui-react";
 
 import { connect } from "react-redux";
 
@@ -7,18 +7,48 @@ import { connect } from "react-redux";
 
 class ArtCard extends Component {
 
+  // when the ArtCard component mounts, it will check the contents of the voteChoicesReducer, []
+  // it will also set state.overlay to props.overlay passed from the landing page
+  // NOTE: there seems to be a race condition happening - 
+  // componentDidMount() {
+  //   this.props.dispatch({
+  //     type: "CHECK_VOTE_CHOICES"
+  //   })
+  //   this.setState({
+  //     overlay: this.props.overlay,
+  //     voteRank: this.props.reduxState.voteChoicesReducer.findIndex(this.findArrayPosition) + 1
+  //   })
+  // }
+
   state = { 
-    overlay: "ui disabled dimmer",
-    voteRank: "" 
+    overlay: "",
+    voteRank: ""
   }
 
-  activateOverlay = () => this.setState({
-    overlay: "ui active dimmer"
-  });
+  // this will activate the dimmer but setting state.overlay to active and updating the voteRank for this card
+  activateOverlay = () => {
+      this.setState({
+        overlay: "ui active dimmer",
+        voteRank: this.props.reduxState.voteChoicesReducer.findIndex(this.findArrayPosition) + 1
+      });
+  }
 
-  disableOverlay = () => this.setState({
-    overlay: "ui disabled dimmer"
-  });
+  // this will disable the dimmer by setting state.overlay to disabled and updating the voteRank for this card
+  disableOverlay = () => {
+    this.setState({
+      overlay: "ui disabled dimmer",
+      voteRank: this.props.reduxState.voteChoicesReducer.findIndex(this.findArrayPosition) + 1
+    });
+  }
+
+  // function that will take in an id (string) and check if it matches the id (integer) of this card
+  findArrayPosition = (id) => {
+    if(id == this.props.item.id){
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   handleVoteClick = (event) => {
     if (this.checkIfClicked(event.target.value)) {
@@ -41,41 +71,39 @@ class ArtCard extends Component {
     }
   };
 
+  // store array in local variable to avoid altering state
+  // this doesn't seem to be creating the updating problem
   removeChoice = (id) => {
-    if (this.props.reduxState.voteChoicesReducer[0] == id) {
-        this.props.reduxState.voteChoicesReducer.shift();
-    } else if (this.props.reduxState.voteChoicesReducer[1] == id) {
-        this.props.reduxState.voteChoicesReducer.splice(1,1);
+    let newState = this.props.reduxState.voteChoicesReducer;
+    if (newState[0] == id) {
+        newState.shift();
+    } else if (newState[1] == id) {
+        newState.splice(1,1);
     } else {
-        this.props.reduxState.voteChoicesReducer.pop();
+        newState.pop();
     }
-    this.props.dispatch({
-        type: "CHECK_VOTE_CHOICES"
-    });
+    this.props.dispatch({ type: "SET_VOTE_CHOICES", payload: newState });
     this.disableOverlay();
-    console.log(this.props.reduxState.voteChoicesReducer);
   };
 
+  // store array in local variable to avoid alter state
   addChoice = (id) => {
-      console.log('this will add', id);
-      this.props.reduxState.voteChoicesReducer.push(id);
-      this.props.dispatch({
-            type: "CHECK_VOTE_CHOICES"
-        });
-      this.activateOverlay();
-        console.log(this.props.reduxState.voteChoicesReducer);
-        if  (this.props.reduxState.voteChoicesReducer.length === 3) {
-            this.props.dispatch({
-                type: "VOTE_SUBMISSION_MODAL_OPEN"
-            })
-        }
-      };
+    let newState = this.props.reduxState.voteChoicesReducer;
+    newState.push(id);
+    this.props.dispatch({ type: "SET_VOTE_CHOICES", payload: newState });
+    this.activateOverlay();
+      if (this.props.reduxState.voteChoicesReducer.length === 3) {
+        this.props.dispatch({
+          type: "VOTE_SUBMISSION_MODAL_OPEN"
+        })
+      }
+  };
 
   render() {
     return (
       <>
         <Card>
-            <div class={this.state.overlay} onClickOutside={this.handleVoteClick}>
+            <div class={this.state.overlay}>
               <div class="content">
                 <h2>{this.state.voteRank}</h2>
               </div>
