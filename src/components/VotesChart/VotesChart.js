f34fa596c318740d83a5c160e0773e14f462c124
dayton brock <dayton.brock@gmail.com>
2020-04-22 18:02:20 -0500

    WIP: create getVotesSaga, VotesChart, working on tallying vote totals

---
+++ src/components/VotesChart/VotesChart.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+
+import { Bar } from "react-chartjs-2";
+
+
+class AdminVotes extends Component {
+
+    componentDidMount() {
+        this.props.dispatch({ type: "GET_ALL_ART" });
+    }
+
+    calculateVoteTotals = ( artistArray ) => {
+
+        voteTotals = [];
+
+        artistArray.forEach( artist => {
+            voteTotals.push(0);
+        }); // creates 0 totals for each artist
+
+        
+
+    }
+
+    voteChartData = () => {
+
+        let artists = [];
+        this.props.reduxState.setArt.forEach( item => {
+            artists.push( item.artist );
+        });
+
+        let voteData = calculateVoteTotals( artists );
+
+
+        const voteData = {
+            labels: artTitlesArray,
+            legend: {
+                display: false
+            },
+            datasets: [{
+                label: "Vote Results",
+                borderWidth: 1,
+                data: voteData
+            }]
+        }
+
+        return voteData;
+    }
+
+    render() {
+        return (
+            <Bar 
+                data={this.voteChartData}/>
+        );
+    }
+}
+
+const mapReduxStateToProps = (reduxState) => ({
+  reduxState,
+});
+
+export default connect(mapReduxStateToProps)(AdminVotes);
