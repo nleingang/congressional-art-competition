445535d993b6691caf11e2dc5bd2000dca236d2a
dayton brock <dayton.brock@gmail.com>
2020-04-22 18:43:28 -0500

    add function calculateVoteTotals

--- src/components/VotesChart/VotesChart.js
+++ src/components/VotesChart/VotesChart.js
@@ -8,42 +8,48 @@ class AdminVotes extends Component {
 
     componentDidMount() {
         this.props.dispatch({ type: "GET_ALL_ART" });
+        this.props.dispatch({ type: "GET_VOTES" });
     }
 
-    calculateVoteTotals = ( artistArray,  ) => {
+    calculateVoteTotals = ( artistArray, votesArray ) => {
 
-        voteTotals = [];
+        let voteTotals = [];
 
         artistArray.forEach( artist => {
             voteTotals.push(0);
         }); // creates 0 totals for each artist
 
+        votesArray.forEach( vote => {
+            voteTotals[ vote.first_place - 1 ] += 3;
+            voteTotals[ vote.second_place - 1 ] += 2;
+            voteTotals[ vote.third_place - 1 ] += 1;
+        }); // tallies points for each vote's first, second, and third choices
 
-
+        return voteTotals;
     }
 
     voteChartData = () => {
 
         const artReducer = this.props.reduxState.setArt;
-        const voteReducer = this.props.reduxState.setVote;
+        const votes = this.props.reduxState.setVotes;
 
         let artists = [];
-        artData.forEach( item => {
+        artReducer.forEach( item => {
             artists.push( item.artist );
         });
 
-        let voteData = calculateVoteTotals( artists );
+        let voteArray = this.calculateVoteTotals( artists, votes );
 
 
         const voteData = {
-            labels: artTitlesArray,
+            labels: artists,
             legend: {
                 display: false
             },
             datasets: [{
                 label: "Vote Results",
                 borderWidth: 1,
-                data: voteData
+                data: voteArray
             }]
         }
 
