806373804b7cdfacdece1c8b2e539cae392cf1fd
Kellen Baker <mkellenbaker@gmail.com>
2020-04-24 10:56:19 -0500

    sort data for csv download

--- src/components/AdminVotes/AdminVotes.js
+++ src/components/AdminVotes/AdminVotes.js
@@ -1,23 +1,82 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import VotesChart from '../VotesChart/VotesChart';
-
+import { CSVLink, CSVDownload } from "react-csv";
+import VotesChart from "../VotesChart/VotesChart";
 
 class AdminVotes extends Component {
-    render() {
-        return (
-            <section class="admin-votes">
-                <h2>Votes</h2>
-                <div class="chart-wrapper">
-                    <VotesChart />
-                </div>
-                <div>
-                    <button class="ui button">Download CSV</button>
-                </div>
-            </section>
-        );
-    }
+  fakeData = [
+    ["firstname", "lastname", "email"],
+    ["Ahmed", "Tomi", "ah@smthing.co.com"],
+    ["Raed", "Labes", "rl@smthing.co.com"],
+    ["Yezzi", "Min l3b", "ymin@cocococo.com"],
+  ];
+
+  sortCSVData = (artReducer, votesReducer) => {
+    let sortedData = [];
+    let columnHeaders = [
+      "Artist ID Number",
+      "Artist",
+      "First Place Votes",
+      "Second Place Votes",
+      "Third Place Votes",
+    ];
+    sortedData.push(columnHeaders);
+     let firstPlaceVotes = [];
+     let secondPlaceVotes = [];
+     let thirdPlaceVotes = [];
+     votesReducer.map((votes) => {
+       firstPlaceVotes.push(votes.first_place);
+       secondPlaceVotes.push(votes.second_place);
+       thirdPlaceVotes.push(votes.third_place);
+     });
+    artReducer.map(object => {
+        let row = [];
+        row.push(object.id);
+        row.push(object.artist);
+        row.push(0);
+        row.push(0);
+        row.push(0);
+        firstPlaceVotes.forEach(vote => {
+            if (vote == row[0]) {
+                row[2]++;
+            }
+        });
+        secondPlaceVotes.forEach(vote => {
+            if(vote == row[0]) {
+                row[3]++;
+            }
+        })
+        thirdPlaceVotes.forEach(vote => {
+            if(vote == row[0]) {
+                row[4]++;
+            }
+        })
+        sortedData.push(row);
+    });
+    return sortedData;
+  };
+
+  render() {
+    return (
+      <section class="admin-votes">
+        <h2>Votes</h2>
+        <div class="chart-wrapper">
+          <VotesChart />
+        </div>
+        <div>
+          <CSVLink
+            data={this.sortCSVData(
+              this.props.reduxState.setArt,
+              this.props.reduxState.setVotes
+            )}
+            class="ui button"
+          >
+            Download CSV
+          </CSVLink>
+        </div>
+      </section>
+    );
+  }
 }
 
 const mapReduxStateToProps = (reduxState) => ({
