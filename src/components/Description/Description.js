a5cf583e6091f4b9c4da3c27de2d2273f841cc4f
Kellen Baker <mkellenbaker@gmail.com>
2020-05-22 10:06:13 -0500

    update description text

--- src/components/Description/Description.js
+++ src/components/Description/Description.js
@@ -5,24 +5,18 @@ class Description extends Component {
   render() {
     return (
       <Segment raised>
-        Art is often the context that helps us understand who we are and where
-        our common ground is. This year, I encourage students to think about our
-        shared country when making their piece. What is America to you?
-        <ul>
-          <li>
-            Visual art competition open to all high schoolers in Minnesota’s
-            Fifth District
-          </li>
-          <li>Voting is open to MN05 residents May 27 through June 2 </li>
-          <li>
-            Winning submission will get two plane tickets to Washington, DC and
-            have their work displayed in the US Capitol for one year
-          </li>
-          <li>
-            On May 5, 2020, we will gather for a reception in honor of these
-            brilliant young artists and announce the winners.
-          </li>
-        </ul>
+        Every year since 1982, each U.S. Representatives selects a piece of
+        visual art to represent their district on display in the U.S. Capitol. I
+        love this competition – art is often the context that helps us
+        understand who we are and where our common ground is. 
+        This year, I encouraged students to think about our shared country when making their
+        piece. What is America to you? 
+        <br></br>
+        <br></br>
+        Voting is open to MN05 residents May 27
+        through June 2. We will announce the winner after voting has closed.
+        <br></br>
+        <br></br>
         Click on the "Cast Your Vote" button below to choose your favorite
         pieces!
       </Segment>
