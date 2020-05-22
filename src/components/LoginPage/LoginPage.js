64f07dc260549abc69aa078330f6ca461a902fdd
Kellen Baker <mkellenbaker@gmail.com>
2020-05-22 12:34:31 -0500

    disable register if admin exists

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,43 +1,46 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class LoginPage extends Component {
   state = {
-    username: '',
-    password: '',
+    username: "",
+    password: "",
   };
+
+  componentDidMount() {
+    this.props.dispatch({
+      type: "CHECK_IF_ADMIN_EXISTS",
+    });
+  }
 
   login = (event) => {
     event.preventDefault();
 
     if (this.state.username && this.state.password) {
       this.props.dispatch({
-        type: 'LOGIN',
+        type: "LOGIN",
         payload: {
           username: this.state.username,
           password: this.state.password,
         },
       });
     } else {
-      this.props.dispatch({ type: 'LOGIN_INPUT_ERROR' });
+      this.props.dispatch({ type: "LOGIN_INPUT_ERROR" });
     }
-  } // end login
+  }; // end login
 
-  handleInputChangeFor = propertyName => (event) => {
+  handleInputChangeFor = (propertyName) => (event) => {
     this.setState({
       [propertyName]: event.target.value,
     });
-  }
+  };
 
   render() {
     return (
       <div>
-        {this.props.errors.loginMessage && (
-          <h2
-            className="alert"
-            role="alert"
-          >
-            {this.props.errors.loginMessage}
+        {this.props.reduxState.errors.loginMessage && (
+          <h2 className="alert" role="alert">
+            {this.props.reduxState.errors.loginMessage}
           </h2>
         )}
         <form onSubmit={this.login}>
@@ -49,7 +52,7 @@ class LoginPage extends Component {
                 type="text"
                 name="username"
                 value={this.state.username}
-                onChange={this.handleInputChangeFor('username')}
+                onChange={this.handleInputChangeFor("username")}
               />
             </label>
           </div>
@@ -60,7 +63,7 @@ class LoginPage extends Component {
                 type="password"
                 name="password"
                 value={this.state.password}
-                onChange={this.handleInputChangeFor('password')}
+                onChange={this.handleInputChangeFor("password")}
               />
             </label>
           </div>
@@ -73,15 +76,21 @@ class LoginPage extends Component {
             />
           </div>
         </form>
-        <center>
-          <button
-            type="button"
-            className="link-button"
-            onClick={() => {this.props.dispatch({type: 'SET_TO_REGISTER_MODE'})}}
-          >
-            Register
-          </button>
-        </center>
+        {this.props.reduxState.disableRegisterReducer == 'true' ? (
+          <></>
+        ) : (
+          <center>
+            <button
+              type="button"
+              className="link-button"
+              onClick={() => {
+                this.props.dispatch({ type: "SET_TO_REGISTER_MODE" });
+              }}
+            >
+              Register
+            </button>
+          </center>
+        )}
       </div>
     );
   }
@@ -90,8 +99,12 @@ class LoginPage extends Component {
 // Instead of taking everything from state, we just want the error messages.
 // if you wanted you could write this code like this:
 // const mapStateToProps = ({errors}) => ({ errors });
-const mapStateToProps = state => ({
-  errors: state.errors,
+// const mapStateToProps = state => ({
+//   errors: state.errors
+// });
+
+const mapReduxStateToProps = (reduxState) => ({
+  reduxState,
 });
 
-export default connect(mapStateToProps)(LoginPage);
+export default connect(mapReduxStateToProps)(LoginPage);
