ef0f87c29d99eab12a1ef7f1659cd0b65af914eb
Kellen <46249876+mkbaker@users.noreply.github.com>
2020-05-27 17:06:43 -0500

    Revert "same thing but different"

--- src/components/S3Uploader/S3Uploader.js
+++ src/components/S3Uploader/S3Uploader.js
@@ -3,11 +3,10 @@ import DropzoneS3Uploader from "react-dropzone-s3-uploader";
 
 export default class S3Uploader extends React.Component {
   
-  
+
   render() {
-    const PORT = process.env.PORT || "http://localhost:5000";
     const uploadOptions = {
-      server: PORT,
+      server: process.env.PORT | "http://localhost:5000",
       signingUrlQueryParams: { uploadType: "avatar" },
     };
     const s3Url = "https://congressional-art-competition.s3.amazonaws.com";
